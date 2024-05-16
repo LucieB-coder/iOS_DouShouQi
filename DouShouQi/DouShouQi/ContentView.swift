a16a578361b739a92428339c891e3a369deba906
etudiant <etudiant@MB-R-110-000-994.local>
2024-05-16 10:31:43 +0200

    :tada: Init project app

---
+++ DouShouQi/DouShouQi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DouShouQi
+//
+//  Created by etudiant on 5/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DouShouQi/DouShouQi/DouShouQiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DouShouQiApp.swift
+//  DouShouQi
+//
+//  Created by etudiant on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct DouShouQiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
