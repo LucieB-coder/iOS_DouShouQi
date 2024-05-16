4dc417e686a7fbc69d38d1b64919a5541a33b0b3
Chloé <chloe.mourgand@etu.uca.fr>
2024-05-16 12:07:20 +0200

    :construction: starting historic page

---
+++ DouShouQi/DouShouQi/Views/Pages/HistoricPage.swift
@@ -0,0 +1,23 @@
+//
+//  HistoricPage.swift
+//  DouShouQi
+//
+//  Created by Chloé MOURGAND on 16/05/2024.
+//
+
+import SwiftUI
+
+struct HistoricPage: View {
+   // var boards : [String]
+    //var selectedBoard : String
+    var body: some View {
+        Text("afaire")
+
+    }
+}
+
+struct HistoricPage_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoricPage()
+    }
+}
