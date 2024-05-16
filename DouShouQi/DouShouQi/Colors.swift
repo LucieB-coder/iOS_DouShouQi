9784363d6f2fd6c2e364cf14337fdc441a63fbb3
etudiant <etudiant@MB-R-110-000-994.local>
2024-05-16 12:06:11 +0200

    :sparkles: TabBar with app colors (may be changing)

---
+++ DouShouQi/DouShouQi/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  DouShouQi
+//
+//  Created by etudiant on 5/16/24.
+//
+
+import Foundation
+import SwiftUI
+
+public struct Colors {
+    static let background = Color("BackgroundColor");
+    static let primary = Color("PinkPrimaryColor");
+}
--- DouShouQi/DouShouQi/ContentView.swift
+++ DouShouQi/DouShouQi/ContentView.swift
@@ -8,15 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor.background
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Text("Page d'accueil")
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Accueil")
+                }
+            Text("Page historique")
+                .tabItem {
+                    Image(systemName: "clock")
+                    Text("Historique")
+                }
+            Text("Page classement")
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("Classement")
+                }
+            Text("Page Profil")
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Profil")
+                }
+            Text("Menu")
+                .tabItem {
+                    Image(systemName: "line.3.horizontal")
+                    Text("Menu")
+                }
         }
-        .padding()
+        .accentColor(Colors.primary)
     }
 }
 
+
+#Preview {
+    ContentView()
+}
 
