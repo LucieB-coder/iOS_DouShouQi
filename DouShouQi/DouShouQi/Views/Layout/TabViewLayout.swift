15d147cfaeda0dd2e7ae2c175f2715a36eb52af0
etudiant <etudiant@MB-R-110-000-994.local>
2024-05-21 10:52:50 +0200

    :recycle: Exctract TabViewLayout into a Layout component

---
+++ DouShouQi/DouShouQi/Views/Layout/TabViewLayout.swift
@@ -0,0 +1,48 @@
+//
+//  SwiftUIView.swift
+//  DouShouQi
+//
+//  Created by etudiant on 5/21/24.
+//
+
+import SwiftUI
+
+struct TabViewLayout: View {
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor.background
+    }
+    var body: some View {
+        TabView {
+            Text("Page d'accueil")
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Accueil")
+                }
+            Text("Page historique")
+                .tabItem {
+                    Image(systemName: "clock")
+                    Text("Historique")
+                }
+            Text("Page classement")
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("Classement")
+                }
+            Text("Page Profil")
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Profil")
+                }
+            Text("Menu")
+                .tabItem {
+                    Image(systemName: "line.3.horizontal")
+                    Text("Menu")
+                }
+        }
+        .accentColor(Colors.primary)
+    }
+}
+
+#Preview {
+    TabViewLayout()
+}
