05afbf25c125839a218e68fde5075e6c70191f1e
lucie.bedouret <lucie.bedouret@etu.uca.fr>
2024-06-14 14:10:38 +0200

    :sparkles: Quit the game confiirmationDialog

---
+++ DouShouQi/DouShouQi/Views/Components/QuitGameButton.swift
@@ -0,0 +1,49 @@
+//
+//  QuitGameButton.swift
+//  DouShouQi
+//
+//  Created by etudiant on 6/14/24.
+//
+
+import SwiftUI
+
+public struct QuitGameButton: View {
+    @State private var confirmationShown = false
+    @Binding public var isShowingGameView: Bool
+    
+    public var body: some View {
+        HStack{
+            Button(
+                action: { confirmationShown = true }
+            ) {
+                Image(systemName: "chevron.left")
+                    .imageScale(.large)
+                    .foregroundStyle(Colors.primary)
+                    .bold()
+                Text("Quit the game")
+                    .foregroundStyle(Colors.primary)
+            }
+            .confirmationDialog(
+                "Quit the game",
+                isPresented: $confirmationShown
+            )
+            {
+                Button("Pursue the game later") {
+                    withAnimation {
+                        self.isShowingGameView = false
+                    }
+                }
+                Button("Do not save the game") {
+                    withAnimation {
+                        self.isShowingGameView = false
+                    }
+                }
+            }
+            .padding(.top, 75)
+            .padding(.horizontal, 5)
+        }
+        .zIndex(3)
+    }
+}
+
+
--- DouShouQi/DouShouQi/Views/Pages/StartGamePage.swift
+++ DouShouQi/DouShouQi/Views/Pages/StartGamePage.swift
@@ -11,7 +11,7 @@ import DouShouQiModel
 struct StartGamePage: View {
     
     @StateObject private var viewModel = StartGameViewModel()
-    @State private var isShowingGameView = false
+    @State public var isShowingGameView = false
 
     var body: some View {
         VStack(alignment: .center){
@@ -41,18 +41,7 @@ struct StartGamePage: View {
             }
             .fullScreenCover(isPresented: $isShowingGameView, content: {
                 ZStack(alignment: .topLeading){
-                    Button(action: {
-                        isShowingGameView = false
-                    }, label: {
-                        HStack{
-                            Image(systemName: "chevron.left")
-                                .foregroundStyle(Colors.primary)
-                            Text("Quit the game")
-                                .foregroundStyle(Colors.primary)
-                        }
-                        .padding()                        
-                    })
-                    .zIndex(3)
+                    QuitGameButton(isShowingGameView: self.$isShowingGameView)
                     SpriteKitGameView(gameViewModel: try! GameViewModel(game: Game(withRules: ClassicRules(), andPlayer1: HumanPlayer(withName: viewModel.player1ViewModel.playerName, andId: .player1)!, andPlayer2: viewModel.player2ViewModel.isBot ? RandomPlayer(withName: "IYAAAAAAAA", andId: .player2)! : HumanPlayer(withName: viewModel.player2ViewModel.playerName, andId: .player2)!), gameScene: GameScene(size: CGSize(width: 940, height: 740))))
                         .zIndex(1)
                 }
